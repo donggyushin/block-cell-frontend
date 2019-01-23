128a755af5135c329223e8210f23a014808d4c4b
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2019-01-23 13:12:28 +0900

    content appear on scrolling down

--- src/Components/BlockCellToken/BlockCellToken.js
+++ src/Components/BlockCellToken/BlockCellToken.js
@@ -1,35 +1,38 @@
 import React from "react";
 import styles from "./styles.module.scss";
 import classNames from "classnames/bind";
+import Fade from "react-reveal/Fade";
 
 const cx = classNames.bind(styles);
 
 const BlockCellToken = () => {
   return (
-    <div className={cx("BlockCellToken")}>
-      <div className={cx("row")}>
-        <span>BLOCK CELL TOKEN</span>
-      </div>
-      <div className={cx("row")}>
-        <div className={cx("column")}>
-          <div className={cx("circle")} />
-          <div className={cx("contents")}>
-            <div className={cx("title")}>
-              <span>블록셀</span>
-            </div>
-            <div className={cx("content")}>
-              <p>
-                Sapien nec sagittis aliquam malesuada bibendum. Quis auctor elit
-                sed vulputate mi sit amet mauris commodo. Nullam eget felis eget
-                nunc lobortis mattis. Semper risus in hendrerit gravida rutrum
-                quisque. Consectetur purus ut faucibus pulv
-              </p>
+    <Fade bottom>
+      <div className={cx("BlockCellToken")}>
+        <div className={cx("row")}>
+          <span>BLOCK CELL TOKEN</span>
+        </div>
+        <div className={cx("row")}>
+          <div className={cx("column")}>
+            <div className={cx("circle")} />
+            <div className={cx("contents")}>
+              <div className={cx("title")}>
+                <span>블록셀</span>
+              </div>
+              <div className={cx("content")}>
+                <p>
+                  Sapien nec sagittis aliquam malesuada bibendum. Quis auctor
+                  elit sed vulputate mi sit amet mauris commodo. Nullam eget
+                  felis eget nunc lobortis mattis. Semper risus in hendrerit
+                  gravida rutrum quisque. Consectetur purus ut faucibus pulv
+                </p>
+              </div>
             </div>
           </div>
+          <div className={cx("column")}>column</div>
         </div>
-        <div className={cx("column")}>column</div>
       </div>
-    </div>
+    </Fade>
   );
 };
 
--- src/Components/IntroPresident/IntroPresident.js
+++ src/Components/IntroPresident/IntroPresident.js
@@ -1,30 +1,34 @@
 import React from "react";
 import styles from "./styles.module.scss";
 import classNames from "classnames/bind";
+import Fade from "react-reveal/Fade";
 
 const cx = classNames.bind(styles);
 
 const IntroPresident = () => {
   return (
-    <div className={cx("IntroPresident")}>
-      <div className={cx("column")}>
-        <div className={cx("row")}>
-          <span>회장 최창식</span>
+    <Fade bottom>
+      <div className={cx("IntroPresident")}>
+        <div className={cx("column")}>
+          <div className={cx("row")}>
+            <span>회장 최창식</span>
+          </div>
+          <div className={cx("row")} />
+          <div className={cx("row")}>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehende
+            </p>
+          </div>
         </div>
-        <div className={cx("row")} />
-        <div className={cx("row")}>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehende
-          </p>
+        <div className={cx("column")}>
+          <img alt={"president"} src={require("../../media/images/leo.jpg")} />
         </div>
       </div>
-      <div className={cx("column")}>
-        <img alt={"president"} src={require("../../media/images/leo.jpg")} />
-      </div>
-    </div>
+    </Fade>
   );
 };
 
