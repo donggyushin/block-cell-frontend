634c86d59adf76afb32ac0ede322e7224fab80b0
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2019-01-30 01:16:38 +0900

    navvar in noticecomponent changed

--- src/Components/NoticeComponents/NavVar/Presenter.js
+++ src/Components/NoticeComponents/NavVar/Presenter.js
@@ -19,52 +19,54 @@ const Presenter = () => {
         </Link>
       </div>
       <div className={cx("column")}>
-        <div className={cx("item-container")}>
-          <Link to={"/about-us"} style={{ textDecoration: "none" }}>
-            <span className={cx("item", "testtest")}>
-              <div>
-                블럭셀은? <i class="fas fa-caret-down" />
-              </div>
-            </span>
-          </Link>
-        </div>
-        <div className={cx("item-container")}>
-          <Link to={"/business-model"} style={{ textDecoration: "none" }}>
+        <div className={cx("item-container-container")}>
+          <div className={cx("item-container")}>
+            <Link to={"/about-us"} style={{ textDecoration: "none" }}>
+              <span className={cx("item", "testtest")}>
+                <div>
+                  블럭셀은? <i class="fas fa-caret-down" />
+                </div>
+              </span>
+            </Link>
+          </div>
+          <div className={cx("item-container")}>
+            <Link to={"/business-model"} style={{ textDecoration: "none" }}>
+              <span className={cx("item")}>
+                <div>
+                  사업모델 <i class="fas fa-caret-down" />
+                </div>
+              </span>
+            </Link>
+          </div>
+          <div className={cx("item-container")}>
+            <Link to={"/notice"} style={{ textDecoration: "none" }}>
+              <span className={cx("item")}>
+                <div>
+                  게시판 <i class="fas fa-caret-down" />
+                </div>
+              </span>
+            </Link>
+          </div>
+          <div className={cx("item-container")}>
+            <Link to={"/way-to-blockcell"} style={{ textDecoration: "none" }}>
+              <span className={cx("item")}>
+                <div>
+                  오시는 길 <i class="fas fa-caret-down" />
+                </div>
+              </span>
+            </Link>
+          </div>
+          <div className={cx("item-container")}>
             <span className={cx("item")}>
               <div>
-                사업모델 <i class="fas fa-caret-down" />
+                <Link to={"/recruit"} style={{ textDecoration: "none" }}>
+                  <span className={cx("span-in-link")}>
+                    채용안내 <i class="fas fa-caret-down" />
+                  </span>
+                </Link>
               </div>
             </span>
-          </Link>
-        </div>
-        <div className={cx("item-container")}>
-          <Link to={"/notice"} style={{ textDecoration: "none" }}>
-            <span className={cx("item")}>
-              <div>
-                게시판 <i class="fas fa-caret-down" />
-              </div>
-            </span>
-          </Link>
-        </div>
-        <div className={cx("item-container")}>
-          <Link to={"/way-to-blockcell"} style={{ textDecoration: "none" }}>
-            <span className={cx("item")}>
-              <div>
-                오시는 길 <i class="fas fa-caret-down" />
-              </div>
-            </span>
-          </Link>
-        </div>
-        <div className={cx("item-container")}>
-          <span className={cx("item")}>
-            <div>
-              <Link to={"/recruit"} style={{ textDecoration: "none" }}>
-                <span className={cx("span-in-link")}>
-                  채용안내 <i class="fas fa-caret-down" />
-                </span>
-              </Link>
-            </div>
-          </span>
+          </div>
         </div>
         <div className={cx("login-join")}>
           <span className={cx("login-join--item")}>로그인</span>|
