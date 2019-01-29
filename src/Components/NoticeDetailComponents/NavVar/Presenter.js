c3614fc8b3afdd589d39411dd51bfd02daa29c2c
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2019-01-30 01:19:06 +0900

    All navigations and the whole page ratio edited

--- src/Components/NoticeDetailComponents/NavVar/Presenter.js
+++ src/Components/NoticeDetailComponents/NavVar/Presenter.js
@@ -2,10 +2,9 @@ import React from "react";
 import styles from "./styles.module.scss";
 import classNames from "classnames/bind";
 import { Link } from "react-router-dom";
-
 const cx = classNames.bind(styles);
 
-const NavVarPresenter = () => {
+const Presenter = () => {
   return (
     <div className={cx("nav-var")}>
       <div className={cx("column")}>
@@ -20,52 +19,54 @@ const NavVarPresenter = () => {
         </Link>
       </div>
       <div className={cx("column")}>
-        <div className={cx("item-container")}>
-          <Link to={"/about-us"} style={{ textDecoration: "none" }}>
-            <span className={cx("item", "testtest")}>
-              <div>
-                블럭셀은? <i class="fas fa-caret-down" />
-              </div>
-            </span>
-          </Link>
-        </div>
-        <div className={cx("item-container")}>
-          <Link to={"/business-model"} style={{ textDecoration: "none" }}>
-            <span className={cx("item")}>
-              <div>
-                사업모델 <i class="fas fa-caret-down" />
-              </div>
-            </span>
-          </Link>
-        </div>
-        <div className={cx("item-container")}>
-          <Link to={"/notice"} style={{ textDecoration: "none" }}>
-            <span className={cx("item")}>
-              <div>
-                게시판 <i class="fas fa-caret-down" />
-              </div>
-            </span>
-          </Link>
-        </div>
-        <div className={cx("item-container")}>
-          <Link to={"/way-to-blockcell"} style={{ textDecoration: "none" }}>
-            <span className={cx("item")}>
-              <div>
-                오시는 길 <i class="fas fa-caret-down" />
-              </div>
-            </span>
-          </Link>
-        </div>
-        <div className={cx("item-container")}>
-          <span className={cx("item")}>
-            <div>
-              <Link to={"/recruit"} style={{ textDecoration: "none" }}>
-                <span className={cx("span-in-link")}>
+        <div className={cx("item-container-container")}>
+          <div className={cx("item-container")}>
+            <Link to={"/about-us"} style={{ textDecoration: "none" }}>
+              <span className={cx("item", "testtest")}>
+                <div>
                   블럭셀은? <i class="fas fa-caret-down" />
-                </span>
-              </Link>
-            </div>
-          </span>
+                </div>
+              </span>
+            </Link>
+          </div>
+          <div className={cx("item-container")}>
+            <Link to={"/business-model"} style={{ textDecoration: "none" }}>
+              <span className={cx("item")}>
+                <div>
+                  사업모델 <i class="fas fa-caret-down" />
+                </div>
+              </span>
+            </Link>
+          </div>
+          <div className={cx("item-container")}>
+            <Link to={"/notice"} style={{ textDecoration: "none" }}>
+              <span className={cx("item")}>
+                <div>
+                  게시판 <i class="fas fa-caret-down" />
+                </div>
+              </span>
+            </Link>
+          </div>
+          <div className={cx("item-container")}>
+            <Link to={"/way-to-blockcell"} style={{ textDecoration: "none" }}>
+              <span className={cx("item")}>
+                <div>
+                  오시는 길 <i class="fas fa-caret-down" />
+                </div>
+              </span>
+            </Link>
+          </div>
+          <div className={cx("item-container")}>
+            <span className={cx("item")}>
+              <div>
+                <Link to={"/recruit"} style={{ textDecoration: "none" }}>
+                  <span className={cx("span-in-link")}>
+                    채용안내 <i class="fas fa-caret-down" />
+                  </span>
+                </Link>
+              </div>
+            </span>
+          </div>
         </div>
         <div className={cx("login-join")}>
           <span className={cx("login-join--item")}>로그인</span>|
@@ -76,4 +77,4 @@ const NavVarPresenter = () => {
   );
 };
 
-export default NavVarPresenter;
+export default Presenter;
